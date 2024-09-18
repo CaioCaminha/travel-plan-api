56c0297fea9f2cae8700703260f8eca87a93177c
Caio Caminha <caio.caminha@globalpay.com>
2024-09-18 05:52:59 -0300

    defining dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val ktor_version = "3.0.0-rc-1"
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -22,11 +24,21 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-status-pages")
     implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-default-headers-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    //DB dependencies
+    implementation("org.postgresql:postgresql:42.1.4")
+    implementation("com.zaxxer:HikariCP:2.3.2")
 }
